390924aff319078a0dc3370eb8ca6db34d2b3952
Erik Rogers <erik@underlogic.tech>
2022-12-02 23:58:49 -0500

    feat: add readonly support to variables

--- Runtime/RuntimeVariable.cs
+++ Runtime/RuntimeVariable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,12 +7,14 @@ namespace UnderLogic.Variables
 {
     public abstract class RuntimeVariable<T> : ScriptableObject
     {
+        [SerializeField] private bool isReadOnly;
         [SerializeField] private T initialValue;
         [SerializeField] private T value;
 
         public event UnityAction<T> ValueChanging; 
         public event UnityAction<T> ValueChanged;
-        
+
+        public bool IsReadOnly => isReadOnly;
         public T InitialValue => initialValue;
 
         public T Value
@@ -24,6 +27,9 @@ namespace UnderLogic.Variables
 
         public void SetValue(T newValue)
         {
+            if (IsReadOnly)
+                throw new InvalidOperationException("Variable is readonly, cannot change value");
+            
             if (EqualityComparer<T>.Default.Equals(newValue, value))
                 return;
 
@@ -35,12 +41,14 @@ namespace UnderLogic.Variables
         public void SetInitial() => SetValue(InitialValue);
         public void SetDefault() => SetValue(default);
 
+        public void MakeReadOnly() => isReadOnly = true;
+
         public void RaiseValueChanging(T newValue) => ValueChanging?.Invoke(newValue);
         public void RaiseValueChanged() => ValueChanged?.Invoke(Value);
 
         public static implicit operator T(RuntimeVariable<T> variable) => variable.value;
 
-        private void OnEnable() => SetInitial();
+        private void OnEnable() => value = initialValue;
         private void OnValidate() => RaiseValueChanged();
     }
 }
